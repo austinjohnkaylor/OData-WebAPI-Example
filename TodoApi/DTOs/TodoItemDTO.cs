cb74d53d1be55c22d97ccb2ba920374810f2f43a
Austin Kaylor <AustinJKaylor@eaton.com>
2024-07-30 14:44:54 -0400

    Adding implicit operators to the TodoItemDTO.cs to be able to convert a TodoItemDTO to a TodoItem entity and back again

--- TodoApi/DTOs/TodoItemDTO.cs
+++ TodoApi/DTOs/TodoItemDTO.cs
@@ -1,8 +1,33 @@
-﻿namespace TodoApi.DTOs;
+﻿using TodoApi.Models;
+
+namespace TodoApi.DTOs;
 
 public class TodoItemDTO
 {
     public long Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+    
+    public static implicit operator TodoItemDTO(Models.TodoItem todoItem)
+    {
+        TodoItemDTO dto = new()
+        {
+            Id = todoItem.Id,
+            Name = todoItem.Name,
+            IsComplete = todoItem.IsComplete
+        };
+        return dto;
+    }
+
+    public static implicit operator TodoItem(TodoItemDTO todoItemDto)
+    {
+        TodoItem todoItem = new()
+        {
+            Id = todoItemDto.Id,
+            Name = todoItemDto.Name,
+            IsComplete = todoItemDto.IsComplete
+        };
+
+        return todoItem;
+    }
 }
