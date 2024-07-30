b269f01c29a167e06592af69c3f51abdac952399
Austin Kaylor <AustinJKaylor@eaton.com>
2024-07-30 14:24:29 -0400

    Adding a model for the TodoItem.cs

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Models;
+/// <summary>
+/// Represents a single to-do item.
+/// </summary>
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
