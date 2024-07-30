f74cf9e4e847c8600fa46052f0cb2147c16f5461
Austin Kaylor <AustinJKaylor@eaton.com>
2024-07-30 15:32:17 -0400

    Adding new entity models for Customer and Order

---
+++ TodoApiWithOData/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoApiWithOData.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Order> Orders { get; set; }
+}
---
+++ TodoApiWithOData/Models/Order.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoApiWithOData.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+}
