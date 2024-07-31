1248a4c42dfab744ad00320637216b291e1fbc69
Austin Kaylor <AustinJKaylor@eaton.com>
2024-07-30 21:15:35 -0400

    Lots of changes

---
+++ BasicCrudApiWithOData/Controllers/CustomersController.cs
@@ -0,0 +1,64 @@
+﻿using BasicCrudApiWithOData.EntityFrameworkCore;
+using BasicCrudApiWithOData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicCrudApiWithOData.Controllers;
+
+public class CustomersController(BasicCrudDbContext db) : ODataController
+{
+    public ActionResult<IQueryable<Customer>> Get()
+    {
+        return Ok(db.Customers);
+    }
+    
+    public ActionResult<Customer> Get([FromRoute] int key)
+    {
+        Customer? customer = db.Customers.SingleOrDefault(d => d.Id == key);
+
+        if (customer == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(customer);
+    }
+    
+    public ActionResult<Customer> Post([FromBody] Customer customer)
+    {
+        db.Customers.Add(customer);
+        db.SaveChanges();
+
+        return Created(customer);
+    }
+    
+    public ActionResult<Customer> Put([FromRoute] int key, [FromBody] Customer customer)
+    {
+        if (key != customer.Id)
+        {
+            return BadRequest();
+        }
+
+        db.Entry(customer).State = EntityState.Modified;
+        db.SaveChanges();
+
+        return Updated(customer);
+    }
+    
+    public ActionResult Delete([FromRoute] int key)
+    {
+        Customer? customer = db.Customers.SingleOrDefault(d => d.Id == key);
+
+        if (customer == null)
+        {
+            return NotFound();
+        }
+
+        db.Customers.Remove(customer);
+        db.SaveChanges();
+
+        return NoContent();
+    }
+}
+
--- BasicCrudApiWithOData/Models/Customer.cs
+++ BasicCrudApiWithOData/Models/Customer.cs
@@ -4,16 +4,16 @@ public class Customer
 {
     public int Id { get; set; }
     public string FirstName { get; set; }
-    public char MiddleInitial { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Address { get; set; }
-    public string City { get; set; }
-    public string State { get; set; }
-    public string ZipCode { get; set; }
-    public string Country { get; set; }
-    public int  Age { get; set; }
+    // public char MiddleInitial { get; set; }
+    // public string LastName { get; set; }
+    // public string Email { get; set; }
+    // public string PhoneNumber { get; set; }
+    // public string Address { get; set; }
+    // public string City { get; set; }
+    // public string State { get; set; }
+    // public string ZipCode { get; set; }
+    // public string Country { get; set; }
+    // public int  Age { get; set; }
     public int CreditLimit { get; set; }
     public DateTime CustomerSince { get; set; }
     public CustomerType CustomerType { get; set; }
